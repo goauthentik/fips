ece06da85b6f69ba38d6a385aa198f97f7b5816f
Jens Langhammer <jens.langhammer@beryju.org>
2025-09-05 03:05:29 +0200

    use ubuntu keyserver

--- debian-fips/build/openssl-fips.sh
+++ debian-fips/build/openssl-fips.sh
@@ -8,9 +8,12 @@ wget https://www.openssl.org/source/openssl-${OPENSSL_FIPS_MODULE_VERSION}.tar.g
     -O openssl.tgz
 wget https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_FIPS_MODULE_VERSION}/openssl-${OPENSSL_FIPS_MODULE_VERSION}.tar.gz.asc \
     -O openssl.sig
-gpg --keyserver hkps://keys.openpgp.org --recv-keys BA5473A2B0587B07FB27CF2D216094DFD0CB81EF
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys BA5473A2B0587B07FB27CF2D216094DFD0CB81EF
+# Legacy key, see https://openssl-library.org/source/
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys EFC0A467D613CB83C7ED6D30D894E2CE8B3D79F5
 gpg --batch --verify openssl.sig openssl.tgz
 gpgconf --kill all
+rm -rf "$GNUPGHOME" openssl.sig
 tar xvf openssl.tgz
 cd $build_root/openssl-${OPENSSL_FIPS_MODULE_VERSION}
 sed -i "s:BUILD_METADATA=:BUILD_METADATA=${OPENSSL_VERSION_SUFFIX}:" VERSION.dat
