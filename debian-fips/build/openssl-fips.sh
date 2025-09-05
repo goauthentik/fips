6b48200f4d1b29e992b960e7ba98d0e7cd5b3263
Jens Langhammer <jens.langhammer@beryju.org>
2025-09-05 02:52:47 +0200

    add gpg to validate sources

--- debian-fips/build/openssl-fips.sh
+++ debian-fips/build/openssl-fips.sh
@@ -3,12 +3,18 @@ set -e -u -o pipefail
 
 source common.sh
 
-# Build FIPS Module
 cd ${build_root}
-wget https://www.openssl.org/source/openssl-${OPENSSL_FIPS_MODULE_VERSION}.tar.gz -O openssl.tgz
+wget https://www.openssl.org/source/openssl-${OPENSSL_FIPS_MODULE_VERSION}.tar.gz \
+    -O openssl.tgz
+wget https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_FIPS_MODULE_VERSION}/openssl-${OPENSSL_FIPS_MODULE_VERSION}.tar.gz.asc \
+    -O openssl.sig
+gpg --keyserver hkps://keys.openpgp.org --recv-keys BA5473A2B0587B07FB27CF2D216094DFD0CB81EF
+gpg --batch --verify openssl.sig openssl.tgz
+gpgconf --kill all
 tar xvf openssl.tgz
 cd $build_root/openssl-${OPENSSL_FIPS_MODULE_VERSION}
 sed -i "s:BUILD_METADATA=:BUILD_METADATA=${OPENSSL_VERSION_SUFFIX}:" VERSION.dat
+# Build FIPS Module
 ./Configure enable-fips
 make -j$(nproc)
 cp ./providers/fips.so ${output_root}
--- debian-fips/build/prepare.sh
+++ debian-fips/build/prepare.sh
@@ -6,5 +6,5 @@ source common.sh
 # Prepare for debian source download
 sed -i -s 's/Types: deb/Types: deb deb-src/g' /etc/apt/sources.list.d/debian.sources
 apt-get update
-apt-get install -y dpkg-dev build-essential wget ca-certificates
+apt-get install -y dpkg-dev build-essential wget ca-certificates gnupg
 apt-get build-dep -y openssl=${OPENSSL_VERSION}
